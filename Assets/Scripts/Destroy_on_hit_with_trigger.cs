8807b749ac9bec5c60beacccb45ce4954e955a23
Thycodeismurder <ville.loytola@outlook.com>
2019-03-15 16:25:03 +0200

    Recycle bin and destroy object added

---
+++ Assets/Scripts/Destroy_on_hit_with_trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour
+{
+    void onCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "trigger")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
