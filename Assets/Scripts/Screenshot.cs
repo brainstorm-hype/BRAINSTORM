34ac0dac4fc3418ee99ac850181bafc0f97519db
Maastaah <jessemakinen@hotmail.com>
2019-03-22 15:30:01 +0200

    screenshot script

---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    private string timeNow = DateTime.Now.ToString("ddMMyyyy-Hmm");
+    private int counter;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            counter++;
+            ScreenCapture.CaptureScreenshot($"screenshot{timeNow}_{counter}.png");
+        }
+    }
+}
